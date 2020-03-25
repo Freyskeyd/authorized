a4d6310fb7b9689e084ec23b8c545c56a599ec23
Freyskeyd <simon.paitrault@gmail.com>
2020-03-25 15:48:10 +0100

    feat(example): implement simple one
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -3,12 +3,34 @@ extern crate authorized;
 use authorized::prelude::*;
 
 #[derive(Debug, Authorized)]
-#[authorized(scope = "")]
 struct SimpleStruct {
-    #[authorized(scope = "reader", default = "String::from("Simple")")]
-    name: String
+    #[authorized(scope = "reader", default = "default_name")]
+    name: String,
 }
 
-fn main() {
+fn default_name() -> String {
+    "Simple".into()
+}
 
+fn main() -> Result<(), AuthorizedError> {
+    let simple = SimpleStruct {
+        name: "A simple struct".into(),
+    };
+
+    println!("The Struct: {:?}", simple);
+
+    let result = Authorizor::authorize(&simple, &"reader")?;
+
+    assert_eq!(result.status, AuthorizationStatus::Authorized);
+
+    println!("=> authorized with reader scope: {:?}", result);
+
+    let result = Authorizor::authorize(&simple, &"failling")?;
+
+    assert_eq!(result.status, AuthorizationStatus::Authorized);
+    assert_eq!(result.inner.name, "Simple");
+
+    println!("=> authorized without reader scope: {:?}", result);
+
+    Ok(())
 }
